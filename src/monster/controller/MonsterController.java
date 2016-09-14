758e994ba7614b9323b8433e0a152aaba7078eab
spal7580 <psaineeeraj@gmail.com>
2016-09-14 09:49:53 -0600

    added Print statements

---
+++ src/monster/controller/MonsterController.java
@@ -0,0 +1,23 @@
+package monster.controller;
+
+import monster.model.MarshmallowMonster;
+
+public class MonsterController
+{
+	private MarshmallowMonster firstMonster;
+	
+	public MonsterController()
+	
+	{
+		firstMonster = new MarshmallowMonster(" Mike ", 342.324,123, false, 2234, 213);
+	}
+	
+	public void start()
+	{
+		System.out.println("Here is my Moonster: "+ firstMonster);
+		System.out.println("My moster has this many eyes:"+ firstMonster.getEyeCount());
+		
+	}
+								
+
+}
